99960e188cc155a0f5cea60670197daaf4d092e1
bolmeoguz <bolmeoguz@gmail.com>
2020-10-26 16:18:16 +0300

    hay

---
+++ Grup22/Grup22/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grup22.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grup22.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            var isAdmin = true;
+
+            if (isAdmin)
+            {
+                return RedirectToAction("Admin");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Grup22/Grup22/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grup22.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public Boolean isAdmin { get; set; }
+    }
+}
